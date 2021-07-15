69a5174676ee1c05c7086572746aec4020d1ade6
Tushar Sharma <000.tushar@gmail.com>
2021-07-14 20:07:15 -0400

    fix issues with impl smell

--- model.py
+++ model.py
@@ -14,3 +14,13 @@ class Method:
         self.type_name = type_name
         self.method_name = method_name
         self.start_line_no = start_line_no
+
+
+class ImplSmell:
+    def __init__(self, project_name, package_name, type_name, method_name, smell_name, cause):
+        self.project_name = project_name
+        self.package_name = package_name
+        self.type_name = type_name
+        self.method_name = method_name
+        self.smell_name = smell_name
+        self.cause = cause.strip('\n')
--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from postprocessor import _get_method_list, _get_type_list
+from postprocessor import _get_method_list, _get_type_list, _get_impl_smells_list
 
 
 def test_method_list():
@@ -7,3 +7,6 @@ def test_method_list():
 
 def test_type_list():
     assert len(_get_type_list('./test_files')) == 77
+
+def test_get_impl_smells_list():
+    assert len(_get_impl_smells_list('./test_files')) == 226

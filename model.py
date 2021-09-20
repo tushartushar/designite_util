51ec126653a317d0dedbd45a37b56b333fa1779f
Tushar Sharma <000.tushar@gmail.com>
2021-09-19 21:45:42 -0300

    fix issues and add detailed diff info

--- model.py
+++ model.py
@@ -18,30 +18,39 @@ class Method:
 
 class ImplSmell:
     def __init__(self, project_name, package_name, type_name, method_name, smell_name, cause, line_no):
-        self.project_name = project_name
+        self.project_name = project_name.strip('\n')
         self.package_name = package_name
         self.type_name = type_name
         self.method_name = method_name
         self.smell_name = smell_name
         self.cause = cause.strip('\n')
-        self.m_start_line_no = line_no
+        self.m_start_line_no = line_no.strip('\n')
         self.matched = False
+
+    def __str__(self):
+        return self.project_name + ', ' + self.package_name + ', ' + self.type_name + ', ' + self.method_name + ', ' + self.smell_name + ', ' + self.cause + ', ' + self.m_start_line_no.strip('\n')
 
 
 class DesignSmell:
     def __init__(self, project_name, package_name, type_name, smell_name, cause):
-        self.project_name = project_name
+        self.project_name = project_name.strip('\n')
         self.package_name = package_name
         self.type_name = type_name
         self.smell_name = smell_name
         self.cause = cause.strip('\n')
         self.matched = False
 
+    def __str__(self):
+        return self.project_name + ', ' + self.package_name + ', ' + self.type_name + ', ' + self.smell_name + ', ' + self.cause
 
 class ArchSmell:
     def __init__(self, project_name, package_name, smell_name, cause):
-        self.project_name = project_name
+        self.project_name = project_name.strip('\n')
         self.package_name = package_name
         self.smell_name = smell_name
         self.cause = cause.strip('\n')
         self.matched = False
+
+    def __str__(self):
+        print('str arch')
+        return self.project_name + ', ' + self.package_name + ', ' + self.smell_name + ', ' + self.cause
--- test_diff.py
+++ test_diff.py
@@ -12,20 +12,36 @@ class Designite_diff_tests(unittest.TestCase):
 
     # both folders are identical
     def test_case1(self):
-        self.assertTrue(process('test_files/case1/1', 'test_files/case1/2'))
+        is_same, diff1, diff2, *_ = process('test_files/case1/1', 'test_files/case1/2')
+        self.assertTrue(is_same)
+        self.assertEqual(len(diff1), 0)
+        self.assertEqual(len(diff2), 0)
 
     # folder 1 has one less arch smell
     def test_case2(self):
-        self.assertFalse(process('test_files/case2/1', 'test_files/case2/2'))
+        is_same, diff1, diff2, *_ = process('test_files/case2/1', 'test_files/case2/2')
+        self.assertFalse(is_same)
+        self.assertEqual(len(diff1), 0)
+        self.assertEqual(len(diff2), 1)
 
     # folder 2 has one arch smell that has shuffled class names in cause
     def test_case3(self):
-        self.assertTrue(process('test_files/case3/1', 'test_files/case3/2'))
+        is_same, diff1, diff2, *_ = process('test_files/case3/1', 'test_files/case3/2')
+        self.assertTrue(is_same)
+        self.assertEqual(len(diff1), 0)
+        self.assertEqual(len(diff2), 0)
+
 
     # folder 1 has one less design smell and folder 2 has three design smells (different) less
     def test_case4(self):
-        self.assertFalse(process('test_files/case4/1', 'test_files/case4/2'))
+        is_same, diff1_a, diff2_a, diff1_d, diff2_d, *_ = process('test_files/case4/1', 'test_files/case4/2')
+        self.assertFalse(is_same)
+        self.assertEqual(len(diff1_d), 3)
+        self.assertEqual(len(diff2_d), 1)
 
     # folder 1 has one less impl smell and folder 2 has two impl smells (different) less
     def test_case5(self):
-        self.assertFalse(process('test_files/case5/1', 'test_files/case5/2'))
+        is_same, diff1_a, diff2_a, diff1_d, diff2_d, diff1_i, diff2_i = process('test_files/case5/1', 'test_files/case5/2')
+        self.assertFalse(is_same)
+        self.assertEqual(len(diff1_i), 2)
+        self.assertEqual(len(diff2_i), 1)

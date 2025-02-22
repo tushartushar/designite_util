8451735fa36b3c40b2073cf6838585cf346ffb08
Tushar Sharma <000.tushar@gmail.com>
2025-02-22 17:26:38 -0400

    update version

--- designiteutil/diff_impl.py
+++ designiteutil/diff_impl.py
@@ -12,7 +12,7 @@ class DiffImplDJ(Diff):
 class DiffImplDpy(Diff):
     def diff(self, path1, path2):
         impl_smell_list1 = _get_impl_smells_list_dpy(path1)
-        print(f'total impl_smells1: {len(impl_smell_list1)}')
+        # print(f'total impl_smells1: {len(impl_smell_list1)}')
         impl_smell_list2 = _get_impl_smells_list_dpy(path2)
-        print(f'total impl_smells2: {len(impl_smell_list2)}')
+        # print(f'total impl_smells2: {len(impl_smell_list2)}')
         return self.diff_impl(impl_smell_list1, impl_smell_list2)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Designite-Util',
-    version='1.0.2',
+    version='1.0.3',
     packages=find_packages(exclude=['tests*']),
     entry_points={
         'console_scripts': [
